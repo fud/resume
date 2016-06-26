3021433ce6f560b3432b7ae8f51a81340c459850
Brenton Bills <fud.fud@gmail.com>
2016-06-26 11:30:37 +1000

    Using grunt-hackmyresume. Playing with json resume themes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,53 +1,22 @@
 module.exports = function(grunt) {
 
-  var hmr = require('hackmyresume');
-
+  'use strict';
+  
   grunt.initConfig({
-    build: {
-      options: {
 
+    hackmyresume: {
+      options: {
+        theme: 'node_modules/jsonresume-theme-classy',
+        pdf: 'phantom'
       },
-      html: {
-        options: {
-          theme: 'node_modules/jsonresume-theme-classy',
-          css: 'embed'
-        },
-        dest: ['current/BrentonBillsResume.html']
-      },
-      singlepagehtml: {
-        options: {
-          theme: 'node_modules/jsonresume-theme-onepage',
-          css: 'embed'
-        },
-        dest: ['current/BrentonBillsSingleResume.html']
-      },
-      markdown: {
-        dest: ['current/BrentonBillsResume.md']
+      main: {
+        src: 'resume.json',
+        dest: 'current/resume.all'
       }
     }
+
   });
 
-  grunt.registerMultiTask('build', 'Building resume', function() {
-
-    var build = new hmr.verbs.build();
-
-    grunt.log.write('Building ' + this.data.dest + ' ');
-
-    build.on('hmr:error', function(ex) {
-      grunt.warn('Failed');
-    });
-
-    build.on('hmr:status', function(ex) {
-      grunt.log.write(".");
-    });
-
-    this.options.errHandler = build;
-    build.invoke.call(build, ['resume.json'], this.data.dest, this.options(), function() { } );
-
-    grunt.log.write(' ').ok();
-  });
-
-  grunt.registerTask('default', ['build']);
-
-
+  grunt.loadNpmTasks('grunt-hackmyresume');
+  grunt.registerTask('default', ['hackmyresume']);
 }
